19c0c9366a75e3598efee4ec8ed211f24a98db98
Vadim Khitrin <me@vkhitrin.com>
2025-04-18 18:25:05 +0300

    chore(arch): Update Configuration

--- os/linux/arch/scripts/99-dump-packages.sh
+++ os/linux/arch/scripts/99-dump-packages.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 [ -n "${BASE_DIR}" ] && cd "${BASE_DIR}"
 
 pacman -Qqm | grep -vE '\-debug$|cider' >./pkglist_aur.txt
-flatpak list --app --columns=application >./pkglist_flatpak.txt
+flatpak list --app --columns=application | grep -vE 'com.system76.Cosmic.BaseApp|vkhitrin' >./pkglist_flatpak.txt
 pacman -Qqe | grep -v "$(pacman -Qqm)" >./pkglist.txt
